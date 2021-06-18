4726fc1ce15b942bf518198fc06e06f86e2bb2ba
Lorenzo Carnevale <lcarnevale@unime.it>
2021-06-18 14:44:27 +0100

    reinforce algorithm from pytoarch modified

--- examples/blife_reinforce.py
+++ examples/blife_reinforce.py
@@ -1,108 +1,142 @@
 import gym
-import argparse
 import numpy as np
-from itertools import count
-
+# import matplotlib.pyplot as plt
+from tqdm import tqdm, trange
+# import pandas as pd
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.distributions import Categorical
 
-
-parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                    help='discount factor (default: 0.99)')
-parser.add_argument('--seed', type=int, default=543, metavar='N',
-                    help='random seed (default: 543)')
-parser.add_argument('--render', action='store_true',
-                    help='render the environment')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='interval between training status logs (default: 10)')
-args = parser.parse_args()
-
-
 env = gym.make('gympy:blife-v0')
-env.seed(args.seed)
-torch.manual_seed(args.seed)
+# env.seed(1)
+torch.manual_seed(1)
 
+#Hyperparameters
+learning_rate = 0.01
+gamma = 0.99
 
 class Policy(nn.Module):
-    def __init__(self, state_space_size, action_space_size):
+    def __init__(self):
         super(Policy, self).__init__()
+        self.state_space = env.observation_space.shape[0]
+        self.action_space = env.action_space.n
+        
+        self.l1 = nn.Linear(self.state_space, 128, bias=False)
+        self.l2 = nn.Linear(128, self.action_space, bias=False)
+        
+        self.gamma = gamma
+        
+        # Episode policy and reward history 
+        self.policy_history = torch.empty((), dtype=torch.float32, device = 'cpu')
+        # self.policy_history = Variable(torch.Tensor()) 
+        self.reward_episode = []
+        # Overall reward and loss history
+        self.reward_history = []
+        self.loss_history = []
 
-        self.affine1 = nn.Linear(state_space_size, 128)
-        self.dropout = nn.Dropout(p=0.6)
-        self.affine2 = nn.Linear(128, action_space_size)
+    def forward(self, x):    
+        model = torch.nn.Sequential(
+            self.l1,
+            nn.Dropout(p=0.6),
+            nn.ReLU(),
+            self.l2,
+            nn.Softmax(dim=-1)
+        )
+        return model(x)
 
-        self.saved_log_probs = []
-        self.rewards = []
-
-    def forward(self, x):
-        x = self.affine1(x)
-        x = self.dropout(x)
-        x = F.relu(x)
-        action_scores = self.affine2(x)
-        return F.softmax(action_scores, dim=1)
-
-
-policy = Policy(env.observation_space.shape[0], env.action_space.n)
-optimizer = optim.Adam(policy.parameters(), lr=1e-2)
-eps = np.finfo(np.float32).eps.item()
-
+policy = Policy()
+optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
 
 def select_action(state):
-    state = torch.from_numpy(state).float().unsqueeze(0)
-    probs = policy(state)
-    m = Categorical(probs)
-    action = m.sample()
-    policy.saved_log_probs.append(m.log_prob(action))
-    return action.item()
+    #Select an action (0 or 1) by running policy model and choosing based on the probabilities in state
+    state = torch.from_numpy(state).type(torch.FloatTensor)
+    state = policy(Variable(state))
+    c = Categorical(state)
+    action = c.sample()
+    
+    # Add log probability of our chosen action to our history    
+    if policy.policy_history.dim() != 0:
+        policy.policy_history = torch.cat([policy.policy_history, c.log_prob(action)])
+    else:
+        policy.policy_history = (c.log_prob(action))
+    return action
 
-
-def finish_episode():
+def update_policy():
     R = 0
-    policy_loss = []
-    returns = []
-    for r in policy.rewards[::-1]:
-        R = r + args.gamma * R
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
-    for log_prob, R in zip(policy.saved_log_probs, returns):
-        policy_loss.append(-log_prob * R)
+    rewards = []
+    
+    # Discount future rewards back to the present using gamma
+    for r in policy.reward_episode[::-1]:
+        R = r + policy.gamma * R
+        rewards.insert(0,R)
+        
+    # Scale rewards
+    rewards = torch.FloatTensor(rewards)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+    
+    # Calculate loss
+    loss = (torch.sum(torch.mul(policy.policy_history, Variable(rewards)).mul(-1), -1))
+    
+    # Update network weights
     optimizer.zero_grad()
-    policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
+    loss.backward()
     optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_log_probs[:]
+    
+    #Save and intialize episode history counters
+    policy.loss_history.append(loss.data[0])
+    policy.reward_history.append(np.sum(policy.reward_episode))
+    policy.policy_history = Variable(torch.Tensor())
+    policy.reward_episode= []
 
 
-def main():
+def main(episodes):
     running_reward = 10
-    for i_episode in count(1):
-        state, ep_reward = env.reset(), 0
-        for t in range(1, 10000):  # Don't infinite loop while learning
+    for episode in range(episodes):
+        state = env.reset() # Reset environment and record the starting state
+        done = False       
+    
+        for time in range(1000):
             action = select_action(state)
-            state, reward, done, _ = env.step(action)
-            if args.render:
-                env.render()
-            policy.rewards.append(reward)
-            ep_reward += reward
+            # Step through environment using chosen action
+            state, reward, done, _ = env.step(action.item())
+
+            # Save reward
+            policy.reward_episode.append(reward)
             if done:
                 break
+        
+        # Used to determine when the environment is solved.
+        running_reward = (running_reward * 0.99) + (time * 0.01)
 
-        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
-        finish_episode()
-        if i_episode % args.log_interval == 0:
-            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                  i_episode, ep_reward, running_reward))
-        # if running_reward > env.spec.reward_threshold:
-        if running_reward > 20:
-            print("Solved! Running reward is now {} and "
-                  "the last episode runs to {} time steps!".format(running_reward, t))
+        update_policy()
+
+        if episode % 1 == 0:
+            print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(episode, time, running_reward))
+
+        if running_reward > env.spec.reward_threshold:
+            print("Solved! Running reward is now {} and the last episode runs to {} time steps!".format(running_reward, time))
             break
 
-if __name__ == '__main__':
-    main()
+episodes = 10
+main(episodes)
+
+# window = int(episodes/20)
+
+# fig, ((ax1), (ax2)) = plt.subplots(2, 1, sharey=True, figsize=[9,9]);
+# rolling_mean = pd.Series(policy.reward_history).rolling(window).mean()
+# std = pd.Series(policy.reward_history).rolling(window).std()
+# ax1.plot(rolling_mean)
+# ax1.fill_between(range(len(policy.reward_history)),rolling_mean-std, rolling_mean+std, color='orange', alpha=0.2)
+# ax1.set_title('Episode Length Moving Average ({}-episode window)'.format(window))
+# ax1.set_xlabel('Episode'); ax1.set_ylabel('Episode Length')
+
+# ax2.plot(policy.reward_history)
+# ax2.set_title('Episode Length')
+# ax2.set_xlabel('Episode'); ax2.set_ylabel('Episode Length')
+
+# fig.tight_layout(pad=2)
+# plt.show()
+# #fig.savefig('results.png')
