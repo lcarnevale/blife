ad50051b635b8326e7f8cf1ca3d7f6c70e0bd554
Lorenzo Carnevale <lcarnevale@unime.it>
2021-06-18 14:44:13 +0100

    reward based on energy and termination on expected runtime

--- gympy/envs/blife.py
+++ gympy/envs/blife.py
@@ -1,4 +1,5 @@
 import gym
+import time
 import random
 import numpy as np
 from gym import error, spaces, utils
@@ -18,14 +19,15 @@ class BatteryLifetimeEnv(gym.Env):
         tbd
 
     Observation:
-        Type: Box(1)
+        Type: Box(4)
         Num     Observation                 Min     Max
-        0       Battery Power Consumption   0 W     20 W
-        1       Network Outbound Traffic    0 B     +inf
-        2       Buffer Size                 0       100
+        0       Battery Voltage             0 V     5 V
+        1       Battery Current             0 A     2 A
+        2       Network Outbound Traffic    0 B     +inf
+        3       Buffer Size                 0       100
 
     Actions:
-        Type: Discrete(2)
+        Type: Discrete(3)
         Num     Action
         0       do nothing
         1       sample any 1s, deliver any 1s (streaming)
@@ -39,8 +41,7 @@ class BatteryLifetimeEnv(gym.Env):
         grather than 2W.
 
     Starting State:
-        Configure the white noise scenario. 
-        It does not start at this time.
+        Configure and start the white noise scenario. 
 
     Episode Termination:
         TODO        
@@ -48,32 +49,33 @@ class BatteryLifetimeEnv(gym.Env):
 
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, num_discrete_actions=2):
+    def __init__(self, battery_capacity=2000, num_discrete_actions=2):
         self.__step_counter = 0
+        self.__battery_capacity = battery_capacity
 
-        # TODO("Make them configurable from file.")
-        min_power_consumption = 0
-        max_power_consumption = 20
-        self.__goal_power_consumption = 2
+        min_battery_voltage = 0
+        max_battery_voltage = 5
+
+        min_battery_current = 0
+        max_battery_current = 2
 
         min_net_outbound = 0
         max_net_outbound = 1000000000
-        self.__goal_net_outbound = 2
 
         min_buffer_size = 0
         max_buffer_size = 100
-        self.__goal_buffer_size = 0
 
-        self.__actions_functions = [
+        self.__perform_action = [
             self.__action_do_nothing,
             self.__action_streaming_one_second,
             self.__action_streaming_ten_second
         ]
 
         self.__sensors = Sensors()
+        self.__max_expected_runtime = 0.0
 
-        low = np.array([min_power_consumption, min_power_consumption], dtype = np.float16)
-        high = np.array([max_power_consumption, max_power_consumption], dtype = np.float16)
+        low = np.array([min_battery_voltage, min_battery_current, min_net_outbound], dtype = np.float16)
+        high = np.array([max_battery_voltage, max_battery_current, max_net_outbound], dtype = np.float16)
 
         self.action_space = spaces.Discrete(num_discrete_actions)
         self.observation_space = spaces.Box(low, high, dtype = np.float16)
@@ -90,7 +92,9 @@ class BatteryLifetimeEnv(gym.Env):
         self.__reset_experiment()
 
         self.__observations = self.__next_observation()
-        self.__power_last = self.__observations[0]
+        voltage = self.__observations[0]
+        current = self.__observations[1]
+        self.__power_last = current * voltage
         return np.array(self.__observations)
 
     def __configure_experiment(self):
@@ -110,7 +114,8 @@ class BatteryLifetimeEnv(gym.Env):
             See the class description for more details.
         """
         return [
-            self.__sensors.get_power(),
+            self.__sensors.get_bus_voltage(),
+            self.__sensors.get_bus_current(),
             self.__sensors.get_net_bytes_sent()
         ]
 
@@ -120,22 +125,37 @@ class BatteryLifetimeEnv(gym.Env):
         Returns:
 
         """
-        print("Moving action %d during step %d" % (action, self.__step_counter))
+        print("tentative to move action %d during step %d" % (action, self.__step_counter))
         done = False
         
-        self.__assert_action_valid(action)
-        self.__actions_functions[action]()
-        self.__observations = self.__next_observation()
-        self.__power = self.__observations[0]
+        # perform action
+        self.__valuate_action(action)
+        self.__perform_action[action]()
+        print("waiting action has effect ...")
+        time.sleep(10)
 
-        reward = self.__reward_function()
-        if self.__step_counter == 100:
-            done = True
+        # collect observation
+        self.__observations = self.__next_observation()
+        voltage = self.__observations[0]
+        current = self.__observations[1]
+        power = current * voltage
+        print("\tpower at step %d: %.3fmA" % (self.__step_counter, power))
+
+        # calculate reward
+        reward = self.__reward_function(power)
+        print("\treward (energy_delta): %.3fmW" % (reward))
+
+        # verify the termination condition
+        expected_runtime = self.__battery_capacity / current
+        self.__set_max_expected_runtime(expected_runtime)
+        done = self.__termination_function(expected_runtime)
+        print("\ttermination: %.3fh (runtime) >= 24h is %s" % (expected_runtime, done))
+        print("\tmax runtime is %.3fh" % (self.__max_expected_runtime))
+        
         self.__step_counter += 1
-        self.__power_last = self.__power
         return np.array(self.__observations), reward, done, {}
     
-    def __assert_action_valid(self, action):
+    def __valuate_action(self, action):
         err_msg = "%r (%s) invalid" % (action, type(action))
         assert self.action_space.contains(action), err_msg
 
@@ -145,12 +165,37 @@ class BatteryLifetimeEnv(gym.Env):
 
     def __action_streaming_one_second(self):
         # print("sample any 1s, deliver any 1s (streaming)")
-        pass
+        self.__scenario.set_rate(1)
 
     def __action_streaming_ten_second(self):
         # print("sample any 10s, deliver any 10s (streaming)")
-        pass
+        self.__scenario.set_rate(10)
 
-    def __reward_function(self):
-        energy_delta = - (self.__power - self.__power_last) * 1
+    def __reward_function(self, power):
+        """Calculate the reward.
+
+        Returns:
+            float representing the energy delta in mJ
+        """
+        energy_delta = - (power - self.__power_last) * 1
         return energy_delta
+
+    def __set_max_expected_runtime(self, expected_runtime):
+        if expected_runtime > self.__max_expected_runtime:
+            self.__max_expected_runtime = expected_runtime
+
+    def __termination_function(self, expected_runtime):
+        """Calculate the termination
+
+        With the battery capacity and average current consumption,
+        you can compute the expected runtime of your project by 
+        solving the equation:
+        Battery capacity (in mAh) / Average current consumption 
+            (in mA) = Hours of expected runtime
+
+        I expect the runtime is at least one day.
+        
+        Returns:
+            bool representing the termination validation
+        """
+        return expected_runtime >= 24
