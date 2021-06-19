de7c04300a65ce344e4b7c930bb3f4c9e50a5eba
Lorenzo Carnevale <lcarnevale@unime.it>
2021-06-19 22:45:57 +0100

    plot the evaluation parameters

--- examples/blife_reinforce.py
+++ examples/blife_reinforce.py
@@ -1,7 +1,7 @@
 import gym
 import numpy as np
-# import matplotlib.pyplot as plt
-import pandas as pd
+import matplotlib.pyplot as plt
+# import pandas as pd
 
 import torch
 import torch.nn as nn
@@ -75,7 +75,7 @@ def update_policy():
     
     # Calculate loss
     loss = (torch.sum(torch.mul(policy.policy_history, Variable(rewards)).mul(-1), -1))
-    
+
     # Update network weights
     optimizer.zero_grad()
     loss.backward()
@@ -84,16 +84,15 @@ def update_policy():
     #Save and intialize episode history counters
     policy.loss_history.append(loss.item())
     policy.reward_history.append(np.sum(policy.reward_episode))
-    policy.policy_history = Variable(torch.Tensor())
+    policy.policy_history = torch.empty((), dtype=torch.float32, device = 'cpu')
     policy.reward_episode= []
 
-
-def main(episodes=10, MAX_STEP_NUM=2):
+def main(episodes=30, MAX_STEP_NUM=30):
     running_reward = 0
     for episode in range(episodes):
         # Reset environment and record the starting state
         state = env.reset()
-        done = False       
+        done = False
     
         for step in range(1, MAX_STEP_NUM+1):
             action = select_action(state)
@@ -104,35 +103,54 @@ def main(episodes=10, MAX_STEP_NUM=2):
             policy.reward_episode.append(reward)
             if done:
                 break
+        reward_episode = np.sum(policy.reward_episode)
         
         # Used to determine when the environment is solved.
         running_reward = (running_reward * gamma) + (step * learning_rate)
 
         update_policy()
+        print('Episode %d\tLast length (steps): %d\tAverage length (steps): %.2f\tReward episode: %.3f\tLoss: %s' % (episode, step, running_reward, reward_episode, policy.loss_history[-1]))
 
-        print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(episode, step, running_reward))
+        env.close()
+
 
         # if running_reward > env.spec.reward_threshold:
         #     print("Solved! Running reward is now {} and the last episode runs to {} step steps!".format(running_reward, step))
         #     break
     
-    # window = int(episodes/20)
+    # window = int(episodes/1)
+    window = 1
+
+    def moving_average(data, window):
+        return np.convolve(data, np.ones(window), 'valid') / window
 
     # fig, ((ax1), (ax2)) = plt.subplots(2, 1, sharey=True, figsize=[9,9])
-    # rolling_mean = pd.Series(policy.reward_history).rolling(window).mean()
-    # std = pd.Series(policy.reward_history).rolling(window).std()
-    # ax1.plot(rolling_mean)
-    # ax1.fill_between(range(len(policy.reward_history)),rolling_mean-std, rolling_mean+std, color='orange', alpha=0.2)
-    # ax1.set_title('Episode Length Moving Average ({}-episode window)'.format(window))
-    # ax1.set_xlabel('Episode'); ax1.set_ylabel('Episode Length')
 
-    # ax2.plot(policy.reward_history)
-    # ax2.set_title('Episode Length')
-    # ax2.set_xlabel('Episode'); ax2.set_ylabel('Episode Length')
+    fig, ((ax1)) = plt.subplots(1, 1, figsize=[9,9])
+    rolling_mean = moving_average(np.array(policy.reward_history), window)
+    std = np.array(policy.reward_history).std()
+    ax1.plot(rolling_mean)
+    ax1.fill_between(range(len(policy.reward_history)),rolling_mean-std, rolling_mean+std, color='orange', alpha=0.2)
+    ax1.set_title('Episode Length Moving Average ({}-episode window)'.format(window))
+    ax1.set_xlabel('Episode')
+    ax1.set_ylabel('Episode Length')
+    fig.tight_layout(pad=2)
+    fig.savefig('mean_average.png')
 
-    # fig.tight_layout(pad=2)
-    # plt.show()
-    # fig.savefig('results.png')
+    fig, ((ax2)) = plt.subplots(1, 1, figsize=[9,9])
+    ax2.plot(policy.reward_history)
+    # ax2.set_title('Reward')
+    ax2.set_xlabel('Episode')
+    ax2.set_ylabel('Reward')
+    fig.tight_layout(pad=2)
+    fig.savefig('reward.png')
+
+    fig, ((ax3)) = plt.subplots(1, 1, figsize=[9,9])
+    ax3.plot(policy.loss_history)
+    ax3.set_xlabel('Episode')
+    ax3.set_ylabel('Loss')
+    fig.tight_layout(pad=2)
+    fig.savefig('loss.png')
 
 
 if __name__ == "__main__":
